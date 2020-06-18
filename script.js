bc382820958ac61e0aa95a2767a608e08e8344a4
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-06-18 00:15:16 -0700

    Generated an incorrect entry alert message if given incorrect text. As well as made alert messages for password generator

--- script.js
+++ script.js
@@ -5,29 +5,53 @@ var lowerc="abcdefghijklmnopqrstuvwxyz";
 var upperc="ABCDEFGHIGJKLMNOPQRSTUVWXYZ";
 var numc = "0123456789";
 var specialc ="!@#$%^&*()+?-_+=~`";
-
-
-
+var opc = ""
 var password = ""
+
+
+
+
 var lengthpw = prompt("Type password length from 8 to 128", "8") * 1;
 var lowUpNumSpecial = prompt("Type the type of characters you want (Lowercase, uppercase, numerical, or special)", "lowercase");
-for(var i = 0; i < lengthpw.length; i++){
-  if(lowUpNumSpecial==="lowercase"){
-    password = 
-  }
-  if (lowUpNumSpecial === "uppercase"){
 
-  }
-  if (lowUpNumSpecial === "numerical"){
-
-  }
-  if (lowUpNumSpecial === "special"){
-
-  }
+if(lowUpNumSpecial==="lowercase"){
+  opc = lowerc 
 }
-  
+if (lowUpNumSpecial === "uppercase"){
+  opc = upperc
+}
+if (lowUpNumSpecial === "numerical"){
+opc = numc
+}
+if (lowUpNumSpecial === "special"){
+  opc = specialc 
+}
+if (opc === ""){
+  alert("You have incorrect criteria entry.")
+}
+else{
 
 
+
+
+
+
+
+//alert(lengthpw)
+
+for(var i = 0; i < lengthpw; i++){
+
+  var num = (Math.random() * opc.length).toFixed(0)
+
+  //alert(num)
+
+   password += opc[num]
+   //alert(password)
+
+}
+ // alert("This is the generated password " + password)
+
+}
 
 
 
