f4277fdaa4626f8ed1b3abc98a447835db37d98b
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-06-17 23:58:02 -0700

    Creating variables for all types of characters

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowerc="abcdefghijklmnopqrstuvwxyz";
+var upperc="ABCDEFGHIGJKLMNOPQRSTUVWXYZ";
+var numc = "0123456789";
+var specialc ="!@#$%^&*()+?-_+=~`";
 
 
-var lengthpw=prompt("Type password length from 8 to 128", "8")
-var lowUpNumSpecial = prompt("Type the type of characters you want (Lowercase, uppercase, numerical, or special)", "lowercase")
+
+var password = ""
+var lengthpw = prompt("Type password length from 8 to 128", "8") * 1;
+var lowUpNumSpecial = prompt("Type the type of characters you want (Lowercase, uppercase, numerical, or special)", "lowercase");
+for(var i = 0; i < lengthpw.length; i++){
+  if(lowUpNumSpecial==="lowercase"){
+    password = 
+  }
+  if (lowUpNumSpecial === "uppercase"){
+
+  }
+  if (lowUpNumSpecial === "numerical"){
+
+  }
+  if (lowUpNumSpecial === "special"){
+
+  }
+}
+  
+
+
+
+
 
 
 
